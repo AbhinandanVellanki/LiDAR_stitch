45d5ae35e5976f8a5368c02785fb6ddf468f71b9
AbhinandanVellanki <abhinandan.vellanki@gmail.com>
2021-05-04 18:24:13 +0800

    changed final format to LAS

--- ply_2_pcd.sh
+++ ply_2_pcd.sh
@@ -3,7 +3,7 @@
 ply_dir=${1}
 scan_name=${1::-4}
 
-echo "Starting to merge ply files"
+echo "Starting to merge ply files..."
 sleep 2
 
 files=""
@@ -14,13 +14,13 @@ done
 
 echo "Got .ply file names, running merger..."
 #merge loaded ply files using CloudCompare command line
-CloudCompare -SILENT -NO_TIMESTAMP -C_EXPORT_FMT PLY ${files} -MERGE_CLOUDS -AUTO_SAVE OFF -SAVE_CLOUDS FILE "${scan_name}_merged.ply"
-echo "Saved merged ply file!"
+CloudCompare -SILENT -NO_TIMESTAMP -C_EXPORT_FMT LAS ${files} -MERGE_CLOUDS -AUTO_SAVE OFF -SAVE_CLOUDS FILE "${scan_name}_merged.ply"
+echo "Saved merged LAS file: ${scan_name}_merged.ply"
 
 #convert ply file to pcd without changing colour
 #pcl_ply2pcd -h ${scan_name}_merged.ply ${scan_name}_pcd.pcd
 #echo "Saved pcd file!"
 
 #convert ply file to while colour pcd
-python3 change_pts_colour_pcd.py ${scan_name}_merged.ply ${scan_name}.pcd
-echo "Saved pcd file!"
+# python3 change_pts_colour_pcd.py ${scan_name}_merged.ply ${scan_name}.pcd
+# echo "Saved pcd file!"
